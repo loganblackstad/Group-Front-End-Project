b26e339699c2c10c5f3f399d3fbe8fe4708d5fca
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-06 13:15:50 -0400

    updated Modal and add widegt feature

--- scripts/add-widget.js
+++ scripts/add-widget.js
@@ -1,23 +1,3 @@
-// let widgets = [];
-// const widgetlist = localStorage.getItem('widgetList')
-// const parsedList = JSON.parse(widgetlist)
-// $('.grid-stack').html(renderWidgets(parsedList));
-
-// function renderWidgets(widgetsArray) {
-//   let widgetsHtmlArray = widgetsArray.map(currentWidget => {
-//     $('.grid-stack').append((`
-//       <div class="grid-stack-item border border-dark" data-gs-x="0" data-gs-y="0" data-gs-width="4" data-gs-height="2" id="quoteDiv">
-//         <div class="grid-stack-item-content">
-//           <div class="d-flex"><p>${currentWidget.title}</p><span class="ml-auto quoteClose">✖️</span></div>
-//           <div id="bored" class="card mt-1"></div>
-//           <div><button class="d-flex btn btn-dark mt-3" id="boredButton">Quote Test</button></div>
-//         </div>
-//       </div>
-//     `));
-//   });
-//   const joined = widgetHtmlArray.join('');
-//   return joined;
-// };
 
 function saveToWidgetlist(widget) {
   let widgetListJSON = localStorage.getItem('widgetList');
@@ -30,28 +10,25 @@ function saveToWidgetlist(widget) {
   localStorage.setItem('widgetList', widgetListJSON);
 }
 
-// $('#addQuotes').on('click', function() {
-//   // e.preventDefault();
-//   console.log("hello world")
-//   let widget = {
-//     "title": "Quote",
-//   };
-//   saveToWidgetlist(widget);
-// })
-
 $('#addQuotes').on('click', function() {
   // e.preventDefault();
   console.log("hello world")
-  // let widget = {
-  //   "title": "Quote",
-  // };
-  $('.grid-stack').after(`
-    <div class="grid-stack-item border border-dark" data-gs-x="0" data-gs-y="0" data-gs-width="4" data-gs-height="2" id="quoteDiv">
-      <div class="grid-stack-item-content">
-        <div class="d-flex"><p>Quote</p><span class="ml-auto quoteClose">✖️</span></div>
-        <div id="bored" class="card mt-1"></div>
-        <div><button class="d-flex btn btn-dark mt-3" id="boredButton">Quote Test</button></div>
-      </div>
-    </div>
-  `);
+  let widget = {
+    "title": "Quote",
+  };
+  let newWidget = renderWidgets(widget);
+  grid.addWidget(newWidget)
 });
+
+function renderWidgets(widget) {
+    let renderedWidget = (`
+      <div class="grid-stack-item border border-dark" data-gs-x="0" data-gs-y="0" data-gs-width="4" data-gs-height="2" id="quoteDiv">
+        <div class="grid-stack-item-content">
+          <div class="d-flex"><p>${widget.title}</p><span class="ml-auto quoteClose">✖️</span></div>
+          <div id="bored" class="card mt-1"></div>
+          <div><button class="d-flex btn btn-dark mt-3" id="boredButton">My new widget</button></div>
+        </div>
+      </div>
+    `);
+    return renderedWidget;
+};
