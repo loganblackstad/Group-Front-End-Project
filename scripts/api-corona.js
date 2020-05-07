7d013256d5f5d64a6f9b99cd74123894de1e8316
Logan Blackstad <logan.blackstad@gmail.com>
2020-05-07 11:02:15 -0400

    changes made to api-corona

--- scripts/api-corona.js
+++ scripts/api-corona.js
@@ -1,7 +1,6 @@
 //https://disease.sh/v2/states/Georgia?yesterday=true
 //countryInfo.flag
 
-<<<<<<< HEAD
 axios
   .get("https://disease.sh/v2/countries/United%20States?yesterday=true#")
   .then((response) => {
@@ -39,41 +38,3 @@ axios
         </center></div>`;
     $("#card-corona").html(coronaUSA);
   });
-=======
-axios.get("https://disease.sh/v2/countries/United%20States?yesterday=true#")
-    .then(response => {
-        console.log(response)
-        const coronaUSA= `
-            <div id="corona"><center>
-                <div>
-                    <h4><em>COVID-19 Daily News</em></h4>
-                </div>
-                <div>
-                    <h1><b>${response.data.country}</b></h1>
-                </div>
-                <div>
-                    <img src="https://corona.lmao.ninja/assets/img/flags/us.png">
-                </div>
-                <div>
-                    <p><b>Cases:</b> ${response.data.cases}</p>
-                </div>
-                <div>
-                    <p><b>Cases Today:</b> ${response.data.todayCases}</p>
-                </div>
-                <div>
-                    <p><b>Deaths:</b> ${response.data.deaths}</p>
-                </div>
-                <div>
-                    <p><b>Deaths Today:</b> ${response.data.todayDeaths}</p>
-                </div> 
-                <div>
-                    <p><b>Recovered:</b> ${response.data.recovered}</p>
-                </div>
-                <div>
-                    <p><b>Total Test:</b> ${response.data.tests}</p>
-                </div>
-            </center></div>
-        `
-        $('#corona').html(coronaUSA);
-    });
->>>>>>> master
