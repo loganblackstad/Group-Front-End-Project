ebf648fc25d0695537a68913a4c26db76d8756cc
Logan Blackstad <logan.blackstad@gmail.com>
2020-05-07 10:22:08 -0400

    updated corona widget UI

--- scripts/api-corona.js
+++ scripts/api-corona.js
@@ -5,36 +5,36 @@ axios
   .get("https://disease.sh/v2/countries/United%20States?yesterday=true#")
   .then((response) => {
     console.log(response);
-    const coronaUSA = `<div>
+    const coronaUSA = `<div><center>
             <img src="https://corona.lmao.ninja/assets/img/flags/us.png">
             <table class="tg">
             <tbody>
             <tr>
                 <td class="tg-0lax">Cases :</td>
-                <td class="tg-0lax">${response.data.cases}</td>
+                <td class="tg-lqy6">${response.data.cases.toLocaleString()}</td>
             </tr>
             <tr>
                 <td class="tg-0lax">Cases (today) :</td>
-                <td class="tg-0lax">${response.data.todayCases}</td>
+                <td class="tg-lqy6">${response.data.todayCases.toLocaleString()}</td>
             </tr>
             <tr>
                 <td class="tg-0lax">Deaths :</td>
-                <td class="tg-0lax">${response.data.deaths}</td>
+                <td class="tg-lqy6">${response.data.deaths.toLocaleString()}</td>
             </tr>
             <tr>
                 <td class="tg-0lax">Deaths (today) :</td>
-                <td class="tg-0lax">${response.data.todayDeaths}</td>
+                <td class="tg-lqy6">${response.data.todayDeaths.toLocaleString()}</td>
             </tr>
             <tr>
                 <td class="tg-0lax">Recovered :</td>
-                <td class="tg-0lax">${response.data.recovered}</td>
+                <td class="tg-lqy6">${response.data.recovered.toLocaleString()}</td>
             </tr>
             <tr>
                 <td class="tg-0lax">Total Tested :</td>
-                <td class="tg-0lax">${response.data.tests}</td>
+                <td class="tg-lqy6">${response.data.tests.toLocaleString()}</td>
             </tr>
             </tbody>
             </table>
-        </div>`;
+        </center></div>`;
     $("#card-corona").html(coronaUSA);
   });
