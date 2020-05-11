f59e5e0c2b71e0186eada359a0528d381a479d48
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-11 10:38:45 -0400

    working on doggo page

--- scripts/api-doggo.js
+++ scripts/api-doggo.js
@@ -2,8 +2,12 @@ $(document).on('click', '#doggoButton', async function (e) {
   e.preventDefault();
   axios.get("https://dog.ceo/api/breeds/image/random")
     .then(response => {
-      $('#doggo').html(`<img src=${response.data.message} style="max-height: 200px" style="max-width: 185px" alt="Picture of doggo">`);
+      $('#doggo').html(`<img src=${response.data.message} style="max-height: 200px; max-width: 185px;" alt="Picture of doggo">`)
     });
-    var contentHeight = $("#doggoDiv").find(".bor").outerHeight();
-    $("#doggoDiv").height(contentHeight);
+    resizeDoggo();
 });
+
+function resizeDoggo() {
+  var contentHeight = $("#doggoDiv").find(".bor").outerHeight();
+  $("#doggoDiv").height(contentHeight);
+}
