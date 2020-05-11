e149db801d22631b861df920a79d3ccae369d902
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-11 11:17:31 -0400

    Doggos working

--- scripts/api-doggo.js
+++ scripts/api-doggo.js
@@ -2,14 +2,14 @@ $(document).on('click', '#doggoButton', async function (e) {
   e.preventDefault();
   let image = $("#doggoDiv").find("img");
   if (image.length === 0) {
-    $('#doggo').html(`<img src="https://picsum.photos/200/185" id="doggoImage" style="max-height: 200px; max-width: 185px;" alt="Picture of doggo">`)
+    $('#doggo').html(`<img src="images/placeholder.png" id="doggoImage" style="max-height: 200px; max-width: 185px;" alt="Picture of doggo">`)
   }
   axios.get("https://dog.ceo/api/breeds/image/random")
     .then(response => {
       $('#doggoImage').attr('src', response.data.message);
+      resizeDoggo();
     });
     // debugger;
-    resizeDoggo();
 });
 
 function resizeDoggo() {
